22413232cd2cb8f1a8a0b96c221ea9f9a15365f1
Frank <jesusc6482@gmail.com>
2025-01-24 17:26:35 -0500

    Feat: Agregando la entidad

---
+++ GestionDeUsuarios/src/main/java/com/example/GestionDeUsuarios/entitys/UserImpl.java
@@ -0,0 +1,27 @@
+package com.example.GestionDeUsuarios.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class UserImpl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String rol;
+}
