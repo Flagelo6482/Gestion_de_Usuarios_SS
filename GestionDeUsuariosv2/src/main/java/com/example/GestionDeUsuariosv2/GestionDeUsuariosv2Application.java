2dec6269952a0540d8d3a251b85abdec14ee354f
Frank <jesusc6482@gmail.com>
2025-02-04 20:54:54 -0500

    Feat: Añadiendo la segunda version del proyecto

---
+++ GestionDeUsuariosv2/src/main/java/com/example/GestionDeUsuariosv2/GestionDeUsuariosv2Application.java
@@ -0,0 +1,13 @@
+package com.example.GestionDeUsuariosv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeUsuariosv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeUsuariosv2Application.class, args);
+	}
+
+}
